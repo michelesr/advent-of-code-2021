e7c9717ff71680147ea9d068b90a807436cda144
Michele Sorcinelli <michelesr@autistici.org>
2021-12-15 14:15:06 +0000

    Add day3 - part 2

--- day-3/main.py
+++ day-3/main.py
@@ -1,9 +1,12 @@
+from collections import Counter
+
 with open("input", "r") as f:
     numbers = [l.strip() for l in f.readlines()]
 
+# part 1
+
 res = []
 
-# part 1
 for i in range(0, len(numbers[0])):
     bits = [x[i] for x in numbers]
     zeros = [x for x in bits if x == '0']
@@ -17,8 +20,38 @@ inv = [(x + 1) % 2 for x in res]
 res = ''.join([str(x) for x in res])
 inv = ''.join([str(x) for x in inv])
 
-a = int(res, 2)
-b = int(inv, 2)
+a, b = int(res, 2), int(inv, 2)
 print(a * b)
 
-# part 2: TODO
+# part 2
+
+def get_bits(numbers, i):
+    return [number[i] for number in numbers]
+
+def get_keep(c, least=False):
+    if least:
+        keep = '0'
+        if c['1'] < c['0']:
+            keep = '1'
+    else:
+        keep = '1'
+        if c['0'] > c['1']:
+            keep = '0'
+    return keep
+
+def check_numbers(numbers, i, least=False):
+    bits = get_bits(numbers, i)
+    c = Counter(bits)
+    keep = get_keep(c, least)
+    return list(filter(lambda e: e[i] == keep, numbers))
+
+def run_check(numbers, least=False):
+    res, i = numbers, 0
+    while len(res) > 1:
+        res = check_numbers(res, i, least)
+        i += 1
+    return res[0]
+
+a, b = run_check(numbers, False), run_check(numbers, True)
+a, b = int(a, 2), int(b, 2)
+print(a * b)
